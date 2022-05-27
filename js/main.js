e50d53c5d1d7e0bbbaf51b40fa23799e3117413c
kmal808 <kjmalley7@gmail.com>
2022-05-26 19:27:02 -1000

    adding files

---
+++ js/main.js
@@ -0,0 +1,42 @@
+document.querySelector('#clickMe').addEventListener('click', makeReq)
+
+// async function makeReq(){
+
+//   const userName = document.querySelector("#userName").value;
+//   const res = await fetch(`/api?student=${userName}`)
+//   const data = await res.json()
+
+//   console.log(data);
+//   document.querySelector("#personName").textContent = data.name
+//   document.querySelector("#personStatus").textContent = data.status
+//   document.querySelector("#personOccupation").textContent = data.currentOccupation
+// }
+
+async function makeReq(){
+
+  const userName = document.querySelector("#userName").value;
+  const res = await fetch(`/api?question=${userName}`)
+  const data = await res.json()
+
+  console.log(data);
+  document.querySelector("#number").textContent = data.number
+  document.querySelector("#question").textContent = data.question
+  document.querySelector("#answer").textContent = data.answer
+}
+
+
+
+
+class BANKI{
+ constructor ([number,question,answer]){
+  this.number = number
+  this.question = question
+  this.answer = answer
+
+  
+
+ }
+
+
+}
+
---
+++ server.js
@@ -0,0 +1,96 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer((req, res) => {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherpage') {
+    fs.readFile('otherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherotherpage') {
+    fs.readFile('otherotherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/api') {
+    if('question' in params){
+      if(params['question']== 'banki1'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          number: "1",
+          question: "What is the mass of the sun?",
+          answer: "A lot."
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student = leon
+      else if(params['question'] == 'banki2'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          number: "2",
+          question: "What is the diameter of the moon?",
+          answer: "A lot also."
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student != leon
+      else if(params['question'] == 'banki3'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          number: "3",
+          question: "Question?",
+          answer: "Answer3"
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+      else{
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          number: "Not answered yet.",
+          question: "Not answered yet.",
+          answer: "Not answered yet."
+        }
+        res.end(JSON.stringify(objToJson));
+      }
+    }//student if
+  }//else if
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
