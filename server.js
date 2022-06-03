172702c581cfe6ff931c847e494d896c027e7a26
Zack Brooks <zibrooks@hotmail.com>
2022-06-02 19:44:57 -0500

    removed application variable from server.js

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const { application } = require('express')
-const express = require('express')
-const app = express()
-const path = require('path')
-const banki = require('./banki')
+const express = require("express");
+const app = express();
+const path = require("path");
+const banki = require("./banki");
 
-app.use(express.static(path.join(__dirname, 'public') ))
+//displays the html and related files to the user
+app.use(express.static(path.join(__dirname, "public")));
 
 //This is just a link to access the banki api for the user
-app.get('/api/banki', (req, res) => {
-    res.json(banki)
-})
+app.get("/api/banki", (req, res) => {
+  res.json(banki);
+});
 
 //Gets name of category entered by user and sends back an array of json objects with questions that only has that category to the getRandom function on main.js
-app.get('/api/banki/category/:id', (req,res) => {
-    res.send(banki.filter(x => x.category == req.params.id))
-})
+app.get("/api/banki/category/:id", (req, res) => {
+  res.send(banki.filter((x) => x.category == req.params.id));
+});
 
 //Gets specific question number entered by user and sends back the json object with that index to the getQuestion function on main.js
-app.get('/api/banki/question/:id', (req,res) => {
-    res.send(banki[req.params.id])
-})
+app.get("/api/banki/question/:id", (req, res) => {
+  res.send(banki[req.params.id]);
+});
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
